151276f6f575ec159fd8cdf9e5905d0829088869
olegrewko <olegrewko@gmail.com>
2023-07-15 01:00:49 +0300

    catalog

--- src/js/main.js
+++ src/js/main.js
@@ -79,7 +79,16 @@ if (document.querySelector('.tab')) {
   });
 }
 
+  // $('.search__tabs-item').on('click', function(e){
 
+  //       e.preventDefault();
+  //       $('.search__tabs-item').removeClass('search__tabs-item--active');
+  //       $('.search__content-item').removeClass('search__content-item--active');
+  //       $(this).addClass('search__tabs-item--active');
+  //       $($(this).attr('href')).addClass('search__content-item--active');
+
+  // });
+      
 
         function myFunction() {
         document.getElementById("myDropdown").classList.toggle("show");
